dff19073583ba6efeebd372e5cfac1341ded0e34
shicheng <alistar.huang@gmail.com>
2025-10-14 22:42:11 -0700

    Initial Commit

---
+++ PostureMonitor/PostureMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureMonitorApp.swift
+//  PostureMonitor
+//
+//  Created by Shicheng Huang on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PostureMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
